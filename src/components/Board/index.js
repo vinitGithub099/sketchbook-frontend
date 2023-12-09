03b5a6e24d155d42aa9d92f2e0049c97d14bdd4d
Vinit Kumbhare <studyvinit99@gmail.com>
2023-12-09 22:19:04 +0530

    initialize project

---
+++ src/components/Board/index.js
@@ -0,0 +1,5 @@
+import styles from "./index.module.css";
+
+export default function Board() {
+  return <div className={styles.boardContainer}>index</div>;
+}
---
+++ src/components/Menu/index.js
@@ -0,0 +1,31 @@
+import {
+  faEraser,
+  faFileArrowDown,
+  faPencil,
+  faRotateLeft,
+  faRotateRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./index.module.css";
+
+export default function Menu() {
+  return (
+    <div className={styles.menuContainer}>
+      <div className={styles.iconsWrapper}>
+        <FontAwesomeIcon icon={faPencil} className={styles.icons} />
+      </div>
+      <div className={styles.iconsWrapper}>
+        <FontAwesomeIcon icon={faEraser} />
+      </div>
+      <div className={styles.iconsWrapper}>
+        <FontAwesomeIcon icon={faRotateLeft} />
+      </div>
+      <div className={styles.iconsWrapper}>
+        <FontAwesomeIcon icon={faRotateRight} />
+      </div>
+      <div className={styles.iconsWrapper}>
+        <FontAwesomeIcon icon={faFileArrowDown} />
+      </div>
+    </div>
+  );
+}
