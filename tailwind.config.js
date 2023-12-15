4dd05dd15a1bd426b7699cf85b64176895fd9f8b
Vinit Kumbhare <studyvinit99@gmail.com>
2023-12-15 20:33:15 +0530

    style modification

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,8 @@ module.exports = {
       },
       colors: {
         background1: "rgba(255,255,255, 0.9)",
-        background2: "rgba(255,255,255, 0.5)",
+        background2: "rgb(241,245,249)",
+        background3: "rgb(226, 232, 240)",
         shadow1:
           "0px 7px 14px rgba(0, 0, 0, .05), 0px 0px 3.12708px rgba(0, 0, 0, .0798), 0px 0px .931014px rgba(0, 0, 0, .1702)",
         shadow2: "0 0 0 1px #4a47b1",
